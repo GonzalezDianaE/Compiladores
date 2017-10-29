cac42106c8268f5c3cbc592606ab2bd357f63195
GonzalezDiana <dianagonzalez22.dg@gmail.com>
2017-10-29 17:18:02 -0300

    Generación de código objeto (sub, plus, div, mod, and, or, not, eqAr, eqLog, neg, minnor, major)

--- Pascal-TDS/assemblyCode.c
+++ Pascal-TDS/assemblyCode.c
@@ -153,60 +153,187 @@ void generateAssembly(ListThreeDir *head){
 
 void generateAdd(OpThreeDir *operation){
 	if(operation->oper1->type == CONSTANT && operation->oper2->type == CONSTANT){
-		printf("	movl $%d, -%d(%%ebp)\n",((operation->oper1->value) + (operation->oper2->value)),((opertation->result->offSet)*REG_SIZE));
+		printf("	movl $%d, -%d(%%ebp)\n",((operation->oper1->value) + (operation->oper2->value)),((operation->result->offSet)*REG_SIZE));
 	} else {
 		printf("	movl -%d(%%ebp),(%%eax)\n",(operation->oper1->offSet)*REG_SIZE);
 		printf("	addl -%d(%%ebp),(%%eax)\n",(operation->oper2->offSet)*REG_SIZE);
-		printf("	movl (%%eax), -%d(%%ebp)\n",(opertation->result->offSet)*REG_SIZE);
+		printf("	movl (%%eax), -%d(%%ebp)\n",(operation->result->offSet)*REG_SIZE);
 	}
 }
 
+/* Genera las lineas de código objeto correspondientes a una operación aritmética substracción. */
 void generateSub(OpThreeDir *operation){
-
+	if(operation->oper1->type == CONSTANT && operation->oper2->type == CONSTANT){
+		printf("	movl $%d, -%d(%%rbp)\n",((operation->oper1->value) - (operation->oper2->value)),((operation->result->offSet)*REG_SIZE));
+	} else {
+		printf("	movl -%d(%%rbp),(%%eax)\n",(operation->oper1->offSet)*REG_SIZE);
+		printf("	imull -%d(%%rbp),(%%eax)\n",(operation->oper2->offSet)*REG_SIZE);
+		printf("	movl (%%eax), -%d(%%rbp)\n",(operation->result->offSet)*REG_SIZE);
+	}
 }
 
+/* Genera las lineas de código objeto correspondientes a una operación aritmética multiplicación. */
 void generatePlus(OpThreeDir *operation){
-
+	if(operation->oper1->type == CONSTANT && operation->oper2->type == CONSTANT){
+		printf("	movl $%d, -%d(%%rbp)\n",((operation->oper1->value) * (operation->oper2->value)),((operation->result->offSet)*REG_SIZE));
+	} else {
+		printf("	movl -%d(%%rbp),(%%eax)\n",(operation->oper1->offSet)*REG_SIZE);
+		printf("	subl -%d(%%rbp),(%%eax)\n",(operation->oper2->offSet)*REG_SIZE);
+		printf("	movl (%%eax), -%d(%%rbp)\n",(operation->result->offSet)*REG_SIZE);
+	}
 }
 
+/* Genera las lineas de código objeto correspondientes a una operación aritmética divisón. */
 void generateDiv(OpThreeDir *operation){
-
+	if(operation->oper1->type == CONSTANT && operation->oper2->type == CONSTANT){	
+		printf("	movl $%d, -%d(%%rbp)\n",((operation->oper1->value) / (operation->oper2->value)),((operation->result->offSet)*REG_SIZE));
+	} else {
+		printf("	movl -%d(%%rbp),(%%eax)\n",(operation->oper1->offSet)*REG_SIZE);
+		printf("  cltd \n"); //extiende %eax para guardar el resto de la division (ver bien esto)
+		printf("	idivl -%d(%%rbp)\n",(operation->oper2->offSet)*REG_SIZE);
+		printf("	movl (%%eax), -%d(%%rbp)\n",(operation->result->offSet)*REG_SIZE);	
+	}
 }
 
+/* Genera las lineas de código objeto correspondientes a una operación aritmética módulo. */
 void generateMod(OpThreeDir *operation){
-
+	if(operation->oper1->type == CONSTANT && operation->oper2->type == CONSTANT){	
+		printf("	movl $%d, -%d(%%rbp)\n",((operation->oper1->value) % (operation->oper2->value)),((operation->result->offSet)*REG_SIZE));
+	} else {
+		printf("	movl -%d(%%rbp),(%%eax)\n",(operation->oper1->offSet)*REG_SIZE);
+		printf("  cltd \n"); //extiende %eax para guardar el resto de la division (ver bien esto)
+		printf("	idivl -%d(%%rbp)\n",(operation->oper2->offSet)*REG_SIZE);
+		//Ver bien esto, porque en realidad la instruccion es una division VER QUE SERIA EL RESULT
+		printf("	movl (%%edx), -%d(%%rbp)\n",(operation->result->offSet)*REG_SIZE);	
+	}
 }
 
+/* Genera las lineas de código objeto correspondientes a una operación lógica and. */
 void generateAnd(OpThreeDir *operation){
-
+	if(operation->oper1->type == CONSTANT && operation->oper2->type == CONSTANT){	
+		if(((operation->oper1->value) && (operation->oper2->value))==0){ //ES FALSE
+			printf("	movb $%d, -%d(%%rbp)\n",0,((operation->result->offSet)*REG_SIZE));
+		} else{
+			printf("	movb $%d, -%d(%%rbp)\n",1,((operation->result->offSet)*REG_SIZE));
+		}
+	} else {
+		//mueve el valor de la primera variable 
+		printf("  movb $%d, -%d(%%rbp)\n", (operation->oper1->value), ((operation->oper1->offSet)*REG_SIZE));
+		//mueve el valor de la segunda variable
+		printf("	movb $%d, -%d(%%rbp)\n",(operation->oper2->value), ((operation->oper2->offSet)*REG_SIZE));
+		//compara el valor de la segunda variable con la primer variable
+		printf("  cmpb $%d, -%d(%%rbp)\n", (operation->oper2->value), ((operation->oper1->offSet)*REG_SIZE));
+	}
 }
 
+/* Genera las lineas de código objeto correspondientes a una operación lógica or. */
 void generateOr(OpThreeDir *operation){
-
+	if(operation->oper1->type == CONSTANT && operation->oper2->type == CONSTANT){	
+		if(((operation->oper1->value) || (operation->oper2->value))==0){ //ES FALSE
+			printf("	movb $%d, -%d(%%rbp)\n",0,((operation->result->offSet)*REG_SIZE));
+		} else{
+			printf("	movb $%d, -%d(%%rbp)\n",1,((operation->result->offSet)*REG_SIZE));
+		}
+	} else {
+		//mueve el valor de la primera variable 
+		printf("  movb $%d, -%d(%%rbp)\n", (operation->oper1->value), ((operation->oper1->offSet)*REG_SIZE));
+		//mueve el valor de la segunda variable
+		printf("	movb $%d, -%d(%%rbp)\n",(operation->oper2->value), ((operation->oper2->offSet)*REG_SIZE));
+		//compara el valor de la segunda variable con la primer variable
+		printf("  cmpb $%d, -%d(%%rbp)\n", (operation->oper2->value), ((operation->oper1->offSet)*REG_SIZE));
+	}
 }
 
+/* Genera las lineas de código objeto correspondientes a una operación lógica not. */
 void generateNot(OpThreeDir *operation){
-
+	if(operation->oper1->type == CONSTANT){	
+		if((operation->oper1->value)==0){ //ES FALSE
+			printf("	movb $%d, -%d(%%rbp)\n",1,((operation->result->offSet)*REG_SIZE));
+		} else{
+			printf("	movb $%d, -%d(%%rbp)\n",0,((operation->result->offSet)*REG_SIZE));
+		}
+	} else {
+		//PREGUNTAR POR INSTRUCCIONES QUE APARECEN
+		printf("  movb $%d, -%d(%%rbp)\n", (operation->oper1->value), ((operation->oper1->offSet)*REG_SIZE));
+		//printf("  movzbl -%d(%%rbp), (%%eax)\n",((operation->oper1->offSet)*REG_SIZE));
+	}
 }
 
+/* Genera las lineas de código objeto correspondientes a una operación aritmética equal. */
 void generateEqAr(OpThreeDir *operation){
-
+	if(operation->oper1->type == CONSTANT && operation->oper2->type == CONSTANT){
+		if((operation->oper1->value) == (operation->oper2->value)){ //es true
+			printf("	movl $%d, -%d(%%rbp)\n",0,((operation->result->offSet)*REG_SIZE));
+		} else{ 
+				printf("	movl $%d, -%d(%%rbp)\n",1,((operation->result->offSet)*REG_SIZE));
+		}
+	} else{
+		printf("	movl $%d, -%d(%%ebp)\n",(operation->oper1->value),((operation->oper1->offSet)*REG_SIZE));
+		printf("	movl $%d, -%d(%%ebp)\n",(operation->oper2->value),((operation->oper2->offSet)*REG_SIZE));
+		printf("  movl -%d(%%ebp), (%%eax)\n", ((operation->oper1->offSet)*REG_SIZE));
+		printf("  cmpl -%d(%%ebp), (%%eax)\n", ((operation->oper2->offSet)*REG_SIZE));
+	}
 }
 
+/* Genera las lineas de código objeto correspondientes a una operación lógica equal. */
 void generateEqLog(OpThreeDir *operation){
-
+	if(operation->oper1->type == CONSTANT && operation->oper2->type == CONSTANT){
+		if(((operation->oper1->value)==0 && (operation->oper2->value))==0){ //ambos son ceros
+			printf("	movb $%d, -%d(%%rbp)\n",0,((operation->result->offSet)*REG_SIZE));
+		}
+		if(((operation->oper1->value)==1 && (operation->oper2->value))==1){ //ambos son unos
+			printf("	movb $%d, -%d(%%rbp)\n",0,((operation->result->offSet)*REG_SIZE));
+		} else{ //ES 0 Y 1 o 1 Y 0
+				printf("	movb $%d, -%d(%%rbp)\n",1,((operation->result->offSet)*REG_SIZE));
+		}
+	} else {
+		//IGUAL QUE NOT PREGUNTAR
+	}
 }
 
+/* Genera las lineas de código objeto correspondientes a una operación aritmética negación. */
 void generateNeg(OpThreeDir *operation){
-
+	if(operation->oper1->type == CONSTANT){	
+		printf("	movl $%d, -%d(%%rbp)\n",(-(operation->oper1->value)),((operation->result->offSet)*REG_SIZE));
+	} else {
+		printf("	movl -%d(%%rbp),(%%eax)\n",(operation->oper1->offSet)*REG_SIZE);
+		printf("	negl (%%eax)\n");
+		printf("	movl (%%eax), -%d(%%rbp)\n",(operation->result->offSet)*REG_SIZE);	
+	}
 }
 
+
+/* Genera las lineas de código objeto correspondientes a una operación aritmética minnor. */
 void generateMinnor(OpThreeDir *operation){
-
+	if(operation->oper1->type == CONSTANT && operation->oper2->type == CONSTANT){
+		if((operation->oper1->value) < (operation->oper2->value)){ //es true
+			printf("	movl $%d, -%d(%%rbp)\n",1,((operation->result->offSet)*REG_SIZE));
+		} else{ 
+				printf("	movl $%d, -%d(%%rbp)\n",0,((operation->result->offSet)*REG_SIZE));
+		}
+	} else{
+		printf("	movl $%d, -%d(%%ebp)\n",(operation->oper1->value),((operation->oper1->offSet)*REG_SIZE));
+		printf("	movl $%d, -%d(%%ebp)\n",(operation->oper2->value),((operation->oper2->offSet)*REG_SIZE));
+		printf("  movl -%d(%%ebp), (%%eax)\n", ((operation->oper1->offSet)*REG_SIZE));
+		printf("  cmpl -%d(%%ebp), (%%eax)\n", ((operation->oper2->offSet)*REG_SIZE));
+	}
 }
 
-void generateMajor(OpThreeDir *operation){
 
+/* Genera las lineas de código objeto correspondientes a una operación aritmética major. */
+void generateMajor(OpThreeDir *operation){
+	if(operation->oper1->type == CONSTANT && operation->oper2->type == CONSTANT){
+		if((operation->oper1->value) > (operation->oper2->value)){ //es true
+			printf("	movl $%d, -%d(%%rbp)\n",1,((operation->result->offSet)*REG_SIZE));
+		} else{ 
+				printf("	movl $%d, -%d(%%rbp)\n",0,((operation->result->offSet)*REG_SIZE));
+		}
+	} else{
+		printf("	movl $%d, -%d(%%ebp)\n",(operation->oper1->value),((operation->oper1->offSet)*REG_SIZE));
+		printf("	movl $%d, -%d(%%ebp)\n",(operation->oper2->value),((operation->oper2->offSet)*REG_SIZE));
+		printf("  movl -%d(%%ebp), (%%eax)\n", ((operation->oper1->offSet)*REG_SIZE));
+		printf("  cmpl -%d(%%ebp), (%%eax)\n", ((operation->oper2->offSet)*REG_SIZE));
+	}
 }
 
 void generateAssign(OpThreeDir *operation){
@@ -214,13 +341,13 @@ void generateAssign(OpThreeDir *operation){
 }
 
 void generateIf(OpThreeDir *operation){
-	printf("	cmpl $0, -%d(%%ebp)\n", (opertation->result->offset)*REG_SIZE);
-	printf("	je %s\n", opertation->oper1->name);
+	printf("	cmpl $0, -%d(%%ebp)\n", (operation->result->offSet)*REG_SIZE);
+	printf("	je %s\n", operation->oper1->name);
 }
 
 void generateWhile(OpThreeDir *operation){
-	printf("	cmpl $0, -%d(%%ebp)\n", (opertation->result->offset)*REG_SIZE);
-	printf("	je %s\n", opertation->oper1->name);
+	printf("	cmpl $0, -%d(%%ebp)\n", (operation->result->offSet)*REG_SIZE);
+	printf("	je %s\n", operation->oper1->name);
 }
 
 void generateLabel(OpThreeDir *operation){
@@ -232,7 +359,7 @@ void generateJump(OpThreeDir *operation){
 }
 
 void generateLoad(OpThreeDir *operation){
-	printf("	movl $%d, -%d(ebp)\n", operation->oper1->value, (operation->result->offset)*REG_SIZE);
+	printf("	movl $%d, -%d(ebp)\n", operation->oper1->value, (operation->result->offSet)*REG_SIZE);
 }
 
 void generateRetInt(OpThreeDir *operation){
